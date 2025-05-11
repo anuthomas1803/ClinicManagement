513fa8eb44175f9da99b008acde6c4e1a890d889
anuth <anuth@192.168.0.8>
2025-05-11 18:45:51 +0530

    iFRAMES

--- 1ClinicManagement/src/main/java/clinicmanagement/Staff_Doctors_MultipleWindowHandling_IFrames.java
+++ 1ClinicManagement/src/main/java/clinicmanagement/Staff_Doctors_MultipleWindowHandling_IFrames.java
@@ -9,13 +9,16 @@ import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
-public class Staff_Doctors_MultipleWindowHandling {
+public class Staff_Doctors_MultipleWindowHandling_IFrames {
 WebDriver driver;
-public  Staff_Doctors_MultipleWindowHandling(WebDriver driver)
+public  Staff_Doctors_MultipleWindowHandling_IFrames(WebDriver driver)
 {
 	this.driver=driver;
 	PageFactory.initElements(driver,this);
@@ -26,6 +29,7 @@ public  Staff_Doctors_MultipleWindowHandling(WebDriver driver)
 @FindBy(xpath="//span[contains(text(),'Doctors')][1]") WebElement doctorsmodule;
 //span[@class="aside-menu-title" and normalize-space()="Doctors"]
 
+@FindBy(id="a077aa5e") WebElement iframe1;
 
 public void newTabStaffModule() throws AWTException
 {
@@ -72,5 +76,14 @@ public void newTabDoctorsModule() throws AWTException
 	// SWITCH TO INDEX 0 WINDOW/PARENT 
 	driver.switchTo().window(handles.get(0));
 	System.out.println("PARENT WINDOW IS " +driver.getTitle());   
+}
+
+@FindBy(xpath="//img[@src='Jmeter720.png']") WebElement imageIniframe;
+public void frames()
+{
+	//now use the switch command
+	driver.switchTo().frame(iframe1);  //FIRST LOCATE THE IFRAME, THEN WITHIN THE IFRAME IS THE IMAGE. LOCATE IT & CLICK IT
+	imageIniframe.click();
+
 }
 }
--- 1ClinicManagement/src/test/java/TestScriptClinic/Staff_Doctors_multipleWindowHandlingTest.java
+++ 1ClinicManagement/src/test/java/TestScriptClinic/Staff_Doctors_multipleWindowHandlingTest.java
@@ -2,11 +2,13 @@ package TestScriptClinic;
 
 import java.awt.AWTException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import clinicmanagement.SigninPage;
 import clinicmanagement.StaffPageAction_RobotClass;
-import clinicmanagement.Staff_Doctors_MultipleWindowHandling;
+import clinicmanagement.Staff_Doctors_MultipleWindowHandling_IFrames;
 
 public class Staff_Doctors_multipleWindowHandlingTest extends Baseclass{
 	
@@ -16,9 +18,21 @@ public class Staff_Doctors_multipleWindowHandlingTest extends Baseclass{
   {
 	  SigninPage  signin = new SigninPage(driver);
 	 signin.enterCredentials("admin@clinic.com","123456");
-	 Staff_Doctors_MultipleWindowHandling window=new Staff_Doctors_MultipleWindowHandling(driver);
+	 Staff_Doctors_MultipleWindowHandling_IFrames window=new Staff_Doctors_MultipleWindowHandling_IFrames(driver);
 	//window.newTabStaffModule();
 	window.newTabDoctorsModule();
+	
 }
+  @Test
+  public void framehandling()
+  {
+	  WebDriver driver = new ChromeDriver();
+	driver.get("https://demo.guru99.com/test/guru99home/");
+	driver.manage().window().maximize();
+		//First find the element using any of locator stratergy
+	 Staff_Doctors_MultipleWindowHandling_IFrames IFrames=new Staff_Doctors_MultipleWindowHandling_IFrames(driver);
+	IFrames.frames(); 
+	  
+  }
 }
 
